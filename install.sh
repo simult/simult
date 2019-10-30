1cb61e2102b5ff1c2ff82d3eeafced4828a792a7
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-10-30 11:07:10 +0300

    improved install and build

--- install.sh
+++ install.sh
@@ -13,22 +13,28 @@ then
 fi
 echo "OS: $os-$arch"
 
+rm -rf /tmp/simult
+rm -rf /tmp/simult.tar.gz
 wget -O /tmp/simult.tar.gz "http://github.com/simult/simult/releases/latest/download/simult-$os-$arch.tar.gz"
-tar -C /usr/local/bin -xvzf /tmp/simult.tar.gz
+mkdir /tmp/simult
+tar -C /tmp/simult -xvzf /tmp/simult.tar.gz
 
 useradd -U -r -p* -d /etc/simult -M -s /bin/false simult || true
 
 mkdir -p /var/log/simult/
 chown simult: /var/log/simult/
 
-cp -f logrotate /etc/logrotate.d/simult
+cp -f /tmp/simult/conf/logrotate /etc/logrotate.d/simult
 
 mkdir -p /etc/simult/
 mkdir -p /etc/simult/ssl/
-cp -n server.yaml /etc/simult/
+cp -n /tmp/simult/conf/server.yaml /etc/simult/
 chown -R simult: /etc/simult/
 
-cp -f simult-server.service /etc/systemd/system/
+cp -f /tmp/simult/conf/simult-server.service /etc/systemd/system/
 systemctl daemon-reload
+
+rm -rf /tmp/simult
+rm -rf /tmp/simult.tar.gz
 
 echo Installed simult
