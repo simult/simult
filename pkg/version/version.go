06aa1b0247b424f1b2e2dbaa7ea468e89b184d8b
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-11-05 15:52:29 +0300

    pkg version godoc

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,10 +5,12 @@ var (
 	build   string
 )
 
+// Version returns version defined by -ldflags
 func Version() string {
 	return version
 }
 
+// Build returns build defined by -ldflags
 func Build() string {
 	return build
 }
