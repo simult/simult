5b5c304a6de781c9b31b4fdbc530b66b7866d376
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-11-07 16:48:38 +0300

    bug fix in docker-build.sh

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -10,16 +10,17 @@ echo "OS: $OS-$ARCH"
 cd $(dirname "$0")
 DIR=`pwd`
 
-cd $DIR/../target/
+TARGETDIR="$DIR/../target"
+mkdir -p "$TARGETDIR"
+cd "$TARGETDIR"
 TARGETDIR=`pwd`
-cd $DIR
+cd "$DIR"
 
-mkdir -p $TARGETDIR/
 docker build -t simult-server:build4linux ..
 docker run --rm -it -v "$TARGETDIR":/target --entrypoint rsync simult-server:build4linux -a /app/ /target/
 echo Copied app to $TARGETDIR/
 
 TARFLAGS="--owner=0 --group=0"
 if [ "$OS" = "darwin" ]; then TARFLAGS="--uid=0 --gid=0"; fi
-tar $TARFLAGS -C $TARGETDIR/ -cvzf $TARGETDIR/simult-linux-$ARCH.tar.gz bin conf
-echo Archived to $TARGETDIR/simult-linux-$ARCH.tar.gz
+tar $TARFLAGS -C "$TARGETDIR/" -cvzf "$TARGETDIR/simult-linux-$ARCH.tar.gz" bin conf
+echo Archived to "$TARGETDIR/simult-linux-$ARCH.tar.gz"
